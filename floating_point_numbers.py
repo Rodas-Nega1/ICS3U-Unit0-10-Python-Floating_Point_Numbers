5db1d70f7ea9566e4445262eb1f9d8382cadec3f
Ubuntu <rodas.nega@mths.ca>
2021-09-28 21:38:18 +0000

    floating point numbers program

---
+++ floating_point_numbers.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Created by: Rodas Nega
+# Created on: Sep 2021
+# This program calculates floating point numbers
+
+
+def main():
+    # this function adds 0.1 with 0.2
+
+    # process
+    answer = 0.1 + 0.2
+
+    # output
+    print(answer)
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
